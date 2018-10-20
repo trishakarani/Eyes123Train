e0d7d777a32cfee5d625aa5461533d37420c22c0
Trisha Karani <trisha.karani@gmail.com>
2018-10-20 16:28:07 -0700

    version with splash screen and icon

---
+++ README.md
@@ -0,0 +1,1 @@
+Trisha Karani - trisha.karani@gmail.com
---
+++ Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Train_Your_Eyes_iOS
+//
+//  Created by Vijay Karani on 10/14/18.
+//  Copyright © 2018 Eyes123Train. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
