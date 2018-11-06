9bd7968e541f4a1f7154da92e15c8cefb4b668d3
Trisha Karani <trisha.karani@gmail.com>
2018-11-05 23:32:01 -0800

    Build 1st screen for Portrait mode

--- Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/OptokineticViewController.swift
+++ Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/OptokineticViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class OptokineticViewController: UIViewController {
+    
+    @IBOutlet weak var colorButton: UIBarButtonItem!
+    var optoKColorStatus : Bool = false
+    var buttonArray: [UIButton] = [UIButton]()
 
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupView()
     }
     
 
@@ -27,4 +33,76 @@ class OptokineticViewController: UIViewController {
     }
     */
 
+    @IBAction func colorButtonPressed(_ sender: UIBarButtonItem) {
+        optoKColorStatus = !optoKColorStatus
+        setupColorButton()
+    }
+
+    func setupView() {
+        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
+            setupLandscapeModeView()
+        }
+        else {
+            setupPortraitModeView()
+        }
+    }
+    
+    func setupLandscapeModeView() {
+    }
+    
+    func setupPortraitModeView() {
+        stackView.axis = .vertical
+        stackView.alignment = .top
+        stackView.distribution = .equalSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        print("Stack View size \(size) from super view size \(stackView.frame.size)")
+        
+        let numBtns = Int(stackView.frame.height / 50)
+        
+        for btnIdx in 0..<numBtns {
+            let yValue = btnIdx * 50
+            let scrnButton = makeButton(yValue: yValue)
+            buttonArray.append(scrnButton)
+            stackView.addArrangedSubview(scrnButton)
+            
+            stackView.addConstraint(NSLayoutConstraint(item: scrnButton, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackView, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1.0, constant: 0.0))
+            stackView.addConstraint(NSLayoutConstraint(item: scrnButton, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackView, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1.0, constant: 0.0))
+        }
+        
+        self.view.addSubview(stackView)
+        
+//        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//        stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        print("Will Transition to size \(size) from super view size \(self.view.frame.size)")
+        
+        if (size.width > self.view.frame.size.width) {
+            print("Landscape")
+        } else {
+            print("Portrait")
+        }
+    }
+    
+    func makeButton(yValue: Int) -> UIButton {
+        let myButton = UIButton(type: UIButton.ButtonType.system)
+//        myButton.frame = CGRect(x: 0, y: yValue, width: 25, height: 20)
+        myButton.backgroundColor = UIColor.black
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.heightAnchor.constraint(lessThanOrEqualToConstant: 15)
+//        myButton.heightAnchor.constraint(equalToConstant: 120.0).isActive = true
+//        myButton.widthAnchor.constraint(equalToConstant: 120.0).isActive = true
+        
+        return myButton
+    }
+    
+    func setupColorButton() {
+        for btn in buttonArray {
+            btn.backgroundColor = optoKColorStatus ? UIColor.red : UIColor.black
+        }
+        colorButton.tintColor = optoKColorStatus ? UIColor.black : UIColor.red
+    }
 }
--- Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/ViewController.swift
+++ Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
     let agreementString = "This is a research app. It is not meant for diagnostic or treatment of any diseases. \r\n\r\nThere have been many medical research carried out to improve the vision of humans. We are using the research work to help the community. The data collected during the research work is going to be used for medical analysis. \r\n\r\nWe will maintain the confidentiality of our customers results, and no personal data will be shared.\r\n\r\nPlease Click on \"I Accept\" for having made aware of the Agreement."
     
     @IBOutlet weak var agreementTerms: UITextView!
@@ -17,12 +17,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.agreementTerms.delegate = self
         
         addAgreementTerms()
     }
-
+    
     @IBAction func acceptButtonPressed(_ sender: UIButton) {
         UserDefaults.standard.set(true, forKey: "TermsAccepted")
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        return false
     }
     
     func addAgreementTerms() {
