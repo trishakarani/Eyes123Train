dd67e272350d860f8c63e2b344d4ab04302a3e41
Trisha Karani <trisha.karani@gmail.com>
2018-11-07 11:20:33 -0800

    Add Core Data and Podfile for Animation

---
+++ Train_Your_Eyes_iOS/Pods/Target Support Files/Pods-Train_Your_Eyes_iOSUITests/Pods-Train_Your_Eyes_iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Train_Your_Eyes_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Train_Your_Eyes_iOSUITests
+@end
--- Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/OptokineticViewController.swift
+++ Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Controller/OptokineticViewController.swift
@@ -13,6 +13,8 @@ class OptokineticViewController: UIViewController {
     @IBOutlet weak var colorButton: UIBarButtonItem!
     var optoKColorStatus : Bool = false
     var buttonArray: [UIButton] = [UIButton]()
+    var timer = Timer()
+    var timerActionCtr : Int = 0
 
     @IBOutlet weak var stackView: UIStackView!
     
@@ -22,6 +24,9 @@ class OptokineticViewController: UIViewController {
         setupView()
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        timer.invalidate()
+    }
 
     /*
     // MARK: - Navigation
@@ -43,14 +48,15 @@ class OptokineticViewController: UIViewController {
             setupLandscapeModeView()
         }
         else {
-            setupPortraitModeView()
+            setupPortraitModeView_Motion2()
         }
+//        timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
     
     func setupLandscapeModeView() {
     }
     
-    func setupPortraitModeView() {
+    func setupPortraitModeView_Motion1() {
         stackView.axis = .vertical
         stackView.alignment = .top
         stackView.distribution = .equalSpacing
@@ -76,6 +82,51 @@ class OptokineticViewController: UIViewController {
 //        stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
     
+    func setupPortraitModeView_Motion2() {
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        print("Stack View size \(size) from super view size \(stackView.frame.size)")
+        
+        let numBtns = Int(stackView.frame.width / 50)
+        
+        for btnIdx in 0..<2 {
+            let yValue = btnIdx * 50
+            let scrnButton = makeButton(yValue: yValue)
+            buttonArray.append(scrnButton)
+            stackView.addArrangedSubview(scrnButton)
+            
+            stackView.addConstraint(NSLayoutConstraint(item: scrnButton, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackView, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1.0, constant: 0.0))
+            stackView.addConstraint(NSLayoutConstraint(item: scrnButton, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackView, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1.0, constant: 0.0))
+        }
+        
+        self.view.addSubview(stackView)
+        
+//        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//        stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    
+    func setupPortraitModeView_Motion3() {
+    }
+    
+    func setupPortraitModeView_Motion4() {
+    }
+
+    func reversePortraitView_Motion1() {
+        
+    }
+    func reversePortraitView_Motion2() {
+        
+    }
+    func reversePortraitView_Motion3() {
+        
+    }
+    func reversePortraitView_Motion4() {
+        
+    }
+
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         print("Will Transition to size \(size) from super view size \(self.view.frame.size)")
@@ -92,7 +143,8 @@ class OptokineticViewController: UIViewController {
 //        myButton.frame = CGRect(x: 0, y: yValue, width: 25, height: 20)
         myButton.backgroundColor = UIColor.black
         myButton.translatesAutoresizingMaskIntoConstraints = false
-        myButton.heightAnchor.constraint(lessThanOrEqualToConstant: 15)
+        //myButton.heightAnchor.constraint(lessThanOrEqualToConstant: 15)
+        myButton.widthAnchor.constraint(lessThanOrEqualToConstant: 15)
 //        myButton.heightAnchor.constraint(equalToConstant: 120.0).isActive = true
 //        myButton.widthAnchor.constraint(equalToConstant: 120.0).isActive = true
         
@@ -104,5 +156,31 @@ class OptokineticViewController: UIViewController {
             btn.backgroundColor = optoKColorStatus ? UIColor.red : UIColor.black
         }
         colorButton.tintColor = optoKColorStatus ? UIColor.black : UIColor.red
+    }
+
+    @objc func timerAction() {
+        print("Timer Action triggered")
+        switch timerActionCtr {
+        case 0:
+            reversePortraitView_Motion1()
+        case 1:
+            setupPortraitModeView_Motion2()
+        case 2:
+            reversePortraitView_Motion2()
+        case 3:
+            setupPortraitModeView_Motion3()
+        case 4:
+            reversePortraitView_Motion3()
+        case 5:
+            setupPortraitModeView_Motion4()
+        case 6:
+            reversePortraitView_Motion4()
+        default:
+            timerActionCtr = 0
+            setupPortraitModeView_Motion1()
+        }
+        timerActionCtr += 1
+        
+        
     }
 }
