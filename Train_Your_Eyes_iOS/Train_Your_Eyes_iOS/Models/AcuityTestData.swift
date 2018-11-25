0e8739297e07109c6a8eabc7541885cff8821085
Trisha Karani <trisha.karani@gmail.com>
2018-11-24 23:57:04 -0800

    Add new acuity tests characters

--- Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Models/AcuityTestData.swift
+++ Train_Your_Eyes_iOS/Train_Your_Eyes_iOS/Models/AcuityTestData.swift
@@ -2,7 +2,7 @@
 //  AcuityTestData.swift
 //  Train_Your_Eyes_iOS
 //
-//  Created by Vijay Karani on 11/23/18.
+//  Created by Trisha Karani on 11/23/18.
 //  Copyright © 2018 Eyes123Train. All rights reserved.
 //
 
@@ -18,24 +18,36 @@ struct AcuityTestData {
 }
 
 let line1Value1  = AcuityTestData(alphValue: "E", correctedVision:"20/200", fontSize: 200, correctIdx: 2, choices: ["A", "O", "E", "U"])
+let line1Value2  = AcuityTestData(alphValue: "H", correctedVision:"20/200", fontSize: 200, correctIdx: 2, choices: ["P", "C", "H", "D"])
 
 let line2Value1  = AcuityTestData(alphValue: "F", correctedVision:"20/100", fontSize: 100, correctIdx: 2, choices: ["E", "I", "F", "B"])
 let line2Value2  = AcuityTestData(alphValue: "P", correctedVision:"20/100", fontSize: 100, correctIdx: 2, choices: ["A", "B", "P", "O"])
+let line2Value3  = AcuityTestData(alphValue: "T", correctedVision:"20/100", fontSize: 100, correctIdx: 2, choices: ["V", "I", "T", "J"])
 
 let line3Value1  = AcuityTestData(alphValue: "T", correctedVision:"20/70", fontSize: 70, correctIdx: 2, choices: ["V", "I", "T", "J"])
 let line3Value2  = AcuityTestData(alphValue: "O", correctedVision:"20/70", fontSize: 70, correctIdx: 2, choices: ["P", "Q", "O", "B"])
 let line3Value3  = AcuityTestData(alphValue: "Z", correctedVision:"20/70", fontSize: 70, correctIdx: 2, choices: ["J", "I", "Z", "P"])
+let line3Value4  = AcuityTestData(alphValue: "E", correctedVision:"20/70", fontSize: 70, correctIdx: 2, choices: ["B", "I", "E", "P"])
+let line3Value5  = AcuityTestData(alphValue: "C", correctedVision:"20/70", fontSize: 70, correctIdx: 2, choices: ["D", "P", "C", "O"])
+let line3Value6  = AcuityTestData(alphValue: "Y", correctedVision:"20/70", fontSize: 70, correctIdx: 2, choices: ["T", "I", "Y", "C"])
 
 let line4Value1  = AcuityTestData(alphValue: "L", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["I", "K", "L", "J"])
 let line4Value2  = AcuityTestData(alphValue: "P", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["Q", "O", "P", "B"])
 let line4Value3  = AcuityTestData(alphValue: "E", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["I", "F", "E", "B"])
 let line4Value4  = AcuityTestData(alphValue: "D", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["C", "B", "D", "Q"])
+let line4Value5  = AcuityTestData(alphValue: "J", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["T", "I", "J", "Y"])
+let line4Value6  = AcuityTestData(alphValue: "Y", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["T", "I", "Y", "C"])
+let line4Value7  = AcuityTestData(alphValue: "K", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["C", "I", "K", "F"])
+let line4Value8  = AcuityTestData(alphValue: "F", correctedVision:"20/50", fontSize: 50, correctIdx: 2, choices: ["E", "B", "F", "Q"])
 
 let line5Value1  = AcuityTestData(alphValue: "P", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["Q", "O", "P", "B"])
 let line5Value2  = AcuityTestData(alphValue: "E", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["B", "I", "E", "P"])
 let line5Value3  = AcuityTestData(alphValue: "C", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["D", "P", "C", "O"])
 let line5Value4  = AcuityTestData(alphValue: "F", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["I", "E", "F", "B"])
 let line5Value5  = AcuityTestData(alphValue: "D", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["O", "C", "D", "B"])
+let line5Value6  = AcuityTestData(alphValue: "L", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["J", "C", "L", "I"])
+let line5Value7  = AcuityTestData(alphValue: "G", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["Q", "C", "G", "D"])
+let line5Value8  = AcuityTestData(alphValue: "M", correctedVision:"20/40", fontSize: 40, correctIdx: 2, choices: ["D", "C", "M", "N"])
 
 let line6Value1  = AcuityTestData(alphValue: "E", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["Q", "O", "E", "B"])
 let line6Value2  = AcuityTestData(alphValue: "D", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["Q", "O", "D", "B"])
@@ -43,6 +55,8 @@ let line6Value3  = AcuityTestData(alphValue: "F", correctedVision:"20/30", fontS
 let line6Value4  = AcuityTestData(alphValue: "C", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["P", "D", "C", "O"])
 let line6Value5  = AcuityTestData(alphValue: "Z", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["I", "J", "Z", "O"])
 let line6Value6  = AcuityTestData(alphValue: "P", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["B", "Q", "P", "O"])
+let line6Value7  = AcuityTestData(alphValue: "R", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["B", "P", "R", "O"])
+let line6Value8  = AcuityTestData(alphValue: "T", correctedVision:"20/30", fontSize: 30, correctIdx: 2, choices: ["C", "J", "T", "I"])
 
 let line7Value1  = AcuityTestData(alphValue: "F", correctedVision:"20/25", fontSize: 25, correctIdx: 2, choices: ["E", "B", "F", "Q"])
 let line7Value2  = AcuityTestData(alphValue: "E", correctedVision:"20/25", fontSize: 25, correctIdx: 2, choices: ["R", "F", "E", "B"])
@@ -51,15 +65,17 @@ let line7Value4  = AcuityTestData(alphValue: "O", correctedVision:"20/25", fontS
 let line7Value5  = AcuityTestData(alphValue: "P", correctedVision:"20/25", fontSize: 25, correctIdx: 2, choices: ["B", "O", "P", "C"])
 let line7Value6  = AcuityTestData(alphValue: "Z", correctedVision:"20/25", fontSize: 25, correctIdx: 2, choices: ["D", "J", "Z", "I"])
 let line7Value7  = AcuityTestData(alphValue: "D", correctedVision:"20/25", fontSize: 25, correctIdx: 2, choices: ["Q", "C", "D", "B"])
+let line7Value8  = AcuityTestData(alphValue: "T", correctedVision:"20/25", fontSize: 25, correctIdx: 2, choices: ["C", "J", "T", "I"])
 
 let line8Value1  = AcuityTestData(alphValue: "D", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["C", "B", "D", "Q"])
-let line8Value2  = AcuityTestData(alphValue: "E", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["F", "B", "E", "C"])
+let line8Value2  = AcuityTestData(alphValue: "J", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["F", "C", "J", "I"])
 let line8Value3  = AcuityTestData(alphValue: "F", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["E", "C", "F", "B"])
 let line8Value4  = AcuityTestData(alphValue: "P", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["E", "B", "P", "Q"])
 let line8Value5  = AcuityTestData(alphValue: "O", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["Q", "C", "O", "P"])
 let line8Value6  = AcuityTestData(alphValue: "T", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["C", "I", "T", "D"])
 let line8Value7  = AcuityTestData(alphValue: "E", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["C", "B", "E", "Q"])
 let line8Value8  = AcuityTestData(alphValue: "C", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["Q", "O", "C", "D"])
+let line8Value9  = AcuityTestData(alphValue: "K", correctedVision:"20/20", fontSize: 20, correctIdx: 2, choices: ["P", "D", "K", "C"])
 
 let line9Value1  = AcuityTestData(alphValue: "L", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["C", "E", "L", "I"])
 let line9Value2  = AcuityTestData(alphValue: "E", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["C", "B", "E", "D"])
@@ -69,6 +85,9 @@ let line9Value5  = AcuityTestData(alphValue: "D", correctedVision:"20/15", fontS
 let line9Value6  = AcuityTestData(alphValue: "P", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["B", "O", "P", "Q"])
 let line9Value7  = AcuityTestData(alphValue: "C", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["D", "B", "C", "O"])
 let line9Value8  = AcuityTestData(alphValue: "T", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["D", "I", "T", "J"])
+let line9Value9  = AcuityTestData(alphValue: "G", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["D", "C", "G", "P"])
+let line9Value10  = AcuityTestData(alphValue: "Z", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["J", "I", "Z", "C"])
+let line9Value11  = AcuityTestData(alphValue: "R", correctedVision:"20/15", fontSize: 15, correctIdx: 2, choices: ["B", "D", "R", "P"])
 
 let line10Value1  = AcuityTestData(alphValue: "F", correctedVision:"20/13", fontSize: 10, correctIdx: 2, choices: ["C", "E", "F", "I"])
 let line10Value2  = AcuityTestData(alphValue: "D", correctedVision:"20/13", fontSize: 10, correctIdx: 2, choices: ["C", "O", "D", "P"])
@@ -90,23 +109,23 @@ let line11Value8  = AcuityTestData(alphValue: "T", correctedVision:"20/10", font
 let line11Value9  = AcuityTestData(alphValue: "D", correctedVision:"20/10", fontSize: 5, correctIdx: 2, choices: ["Q", "O", "D", "P"])
 
 
-let vision200Values = [line1Value1]
+let vision200Values = [line1Value1, line1Value2]
 
-let vision100Values = [line2Value1, line2Value2]
+let vision100Values = [line2Value1, line2Value2, line2Value3]
 
-let vision70Values = [line3Value1, line3Value2, line3Value3]
+let vision70Values = [line3Value1, line3Value2, line3Value3, line3Value4, line3Value5, line3Value6]
 
-let vision50Values = [line4Value1, line4Value2, line4Value3, line4Value4]
+let vision50Values = [line4Value1, line4Value2, line4Value3, line4Value4, line4Value5, line4Value6, line4Value7, line4Value8]
 
-let vision40Values = [line5Value1, line5Value2, line5Value3, line5Value4, line5Value5]
+let vision40Values = [line5Value1, line5Value2, line5Value3, line5Value4, line5Value5, line5Value6, line5Value7, line5Value8]
 
-let vision30Values = [line6Value1, line6Value2, line6Value3, line6Value4, line6Value5, line6Value6]
+let vision30Values = [line6Value1, line6Value2, line6Value3, line6Value4, line6Value5, line6Value6, line6Value7, line6Value8]
 
-let vision25Values = [line7Value1, line7Value2, line7Value3, line7Value4, line7Value5, line7Value6, line7Value7]
+let vision25Values = [line7Value1, line7Value2, line7Value3, line7Value4, line7Value5, line7Value6, line7Value7, line7Value8]
 
-let vision20Values = [line8Value1, line8Value2, line8Value3, line8Value4, line8Value5, line8Value6, line8Value7, line8Value8]
+let vision20Values = [line8Value1, line8Value2, line8Value3, line8Value4, line8Value5, line8Value6, line8Value7, line8Value8, line8Value9]
 
-let vision15Values = [line9Value1, line9Value2, line9Value3, line9Value4, line9Value5, line9Value6, line9Value7, line9Value8]
+let vision15Values = [line9Value1, line9Value2, line9Value3, line9Value4, line9Value5, line9Value6, line9Value7, line9Value8, line9Value9, line9Value10, line9Value11]
 
 let vision10Values = [line10Value1, line10Value2, line10Value3, line10Value4, line10Value5, line10Value6, line10Value7, line10Value8]
 
